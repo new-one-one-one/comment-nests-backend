b0fec4d17d26bca1ef27c2333184872026cdf426
new-one-one-one <ohri.piyush0190@gmail.com>
2023-11-08 10:20:06 +0530

    added logging support

---
+++ helpers/logger.js
@@ -0,0 +1,34 @@
+const winston = require('winston');
+const fs = require('fs');
+const path = require('path');
+
+const { createLogger, format, transports } = winston;
+const { combine, timestamp, printf } = format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}]: ${message}`;
+});
+
+const logDir = process.env.LOG_DIR || "../logs";
+const date = new Date();
+const year = date.getFullYear();
+const month = date.toLocaleString('default', { month: 'long' });
+const day = date.getDate();
+const logPath = path.join(logDir, String(year), String(month), String(day));
+
+if (!fs.existsSync(logPath)) {
+  fs.mkdirSync(logPath, { recursive: true });
+}
+
+const logger = createLogger({
+  format: combine(timestamp(), logFormat),
+  // Supporting 4 tupes of formats that can be accessible using logger object
+  transports: [
+    new transports.File({ filename: path.join(logPath, 'error.log'), level: 'error' }),
+    new transports.File({ filename: path.join(logPath, 'info.log'), level: 'info' }),
+    new transports.File({ filename: path.join(logPath, 'success.log'), level: 'success' }),
+    new transports.File({ filename: path.join(logPath, 'warning.log'), level: 'warning' }),
+  ],
+});
+
+module.exports = logger;
--- index.js
+++ index.js
@@ -6,31 +6,30 @@ const app = express();
 require('dotenv').config()
 app.use(bodyParser.json());
 
+const logger = require("./helpers/logger");
+
 /**
  * 
  * Testing out mongoDB connectins 
  */
 
-mongoose.connect(process.env.MONGO_DB_STRING_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_DB_STRING_URI);
 
 mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
+  logger.info('Connected to MongoDB');
 });
 
 mongoose.connection.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
+  logger.error('MongoDB connection error:', err);
 });
 
 mongoose.connection.on('disconnected', () => {
-  console.log('MongoDB connection disconnected');
+  logger.info('MongoDB connection disconnected');
 });
 
 process.on('SIGINT', () => {
   mongoose.connection.close(() => {
-    console.log('MongoDB connection disconnected through app termination');
+    logger.info('MongoDB connection disconnected through app termination');
     process.exit(0);
   });
 });
@@ -40,15 +39,19 @@ process.on('SIGINT', () => {
     const userModel = mongoose.models.User;
     
     if (userModel) {
-      console.log('User model is registered in the database.');
+      logger.info('User model is registered in the database.');
     } else {
-      console.log('User model is not registered in the database.');
+      logger.info('User model is not registered in the database.');
     }
     
 })()
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, (err) => {
+    if(err) {
+        logger.error(JSON.stringify(err))
+        process.exit(0);
+    }
+    logger.info(`Server is running on port ${PORT}`);
 });
